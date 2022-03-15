b16472c6a2c2c3012431403cc3f1227f73f73d94
Anton <goncharuk.official@gmail.com>
2022-03-15 17:24:44 +0300

    Refactoring

--- Lesson2/amount.rb
+++ Lesson2/amount.rb
@@ -1,4 +1,4 @@
-base = Hash.new # База товаров
+base = {} # База товаров
 puts "Введите название товара:"
 product_name = gets.chomp
 puts "Введите цену за единицу товара:"
@@ -8,28 +8,29 @@ quantity = gets.chomp.to_i
 base[product_name] = {price_per_unit => quantity }
 puts base
 base[product_name].each do |price, quantity|
- sum =  price * quantity
-puts "Общая сумма #{product_name}  равна #{sum}"
+ sum =  price * quantrity
+puts "Общая сумма #{product_name} равна #{sum}"
+end 
 
 loop do
-  puts "Введите название товара:"
-  product_name = gets.chomp
-  break if product_name == "стоп"
-  puts "Введите цену за единицу товара:"
-  price_per_unit = gets.chomp.to_i
-  puts "Введите количество:"
-  quantity = gets.chomp.to_i
-  base[product_name] = {price_per_unit => quantity }
-  puts base
-  base[product_name].each do |price, quantity|
-   sum =  price * quantity
+ puts "Введите название товара:"
+ product_name = gets.chomp
+ break if product_name == "стоп"
+ puts "Введите цену за единицу товара:"
+ price_per_unit = gets.chomp.to_i
+ puts "Введите количество:"
+ quantity = gets.chomp.to_i
+ base[product_name] = {price_per_unit => quantity }
+ puts base
+ base[product_name].each do |price, quantity|
+  sum =  price * quantity
   puts "Общая сумма #{product_name}  равна #{sum}"
-  end
+ end
 end
 
 base.each do | product, p_q| # p_q это хэш прайс + количество
-    p_q.each  do |p, q|
-      amount = p * q
-      puts "Общая сумма товаров равна #{amount}"
-    end
+ p_q.each  do |p, q|
+  amount = p * q
+  puts "Общая сумма товаров  #{product} равна #{amount}"
+ end
 end
--- Lesson2/hash_month.rb
+++ Lesson2/hash_month.rb
@@ -1,7 +1,5 @@
 months = { "january" => 31, "february" => 29, "marth" => 31, "april" => 30,
 "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, "october" => 31, "november" => 30,"desember" => 31}
 months.each { |month, days|
-  if days == 30
-    puts month
-  end
+ puts month if days == 30
 }
