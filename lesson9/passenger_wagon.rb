6c2778b9dbf53c0928265bfbbbb6aafd35e9ad45
Anton <goncharuk.official@gmail.com>
2022-06-01 16:17:14 +0300

    Модули Validate и accessor

---
+++ lesson9/passenger_wagon.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class PassengerWagon < Wagon
+  attr_reader :seats, :num_seats, :free_seats
+
+  def initialize(number, num_seats)
+    @num_seats = num_seats
+    @free_seats = num_seats
+    @seats = []
+    @type = 'passenger'
+    super(number)
+  end
+
+  def take_a_seat(num)
+    if num.positive? && num <= num_seats
+      seats.push(num)
+      @free_seats = num_seats.to_i - @seats.length
+    else
+      puts 'Такого места нет в поезде'
+    end
+  end
+
+  def occupied_seats
+    puts "Занято мест в поезде: #{@seats.length}"
+    puts "Свободных мест в поезде: #{@free_seats}"
+  end
+
+  def validate!
+    super
+    raise 'Количество мест не было введено' if num_seats.nil?
+    raise 'Некорректно введено количество мест' if num_seats.zero?
+  end
+end
---
+++ lesson9/train.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+class Train
+  include ManufacturerName
+  include InstanceCounter
+  include Acсessors
+  include Validation
+
+  @@all_trains = {}
+  NUMBER_FORMAT = /^[а-я]{3}|\d{3}-*[а-я]|\d{2}$/i.freeze
+
+  attr_reader :speed, :type, :route, :number, :current_station, :wagons_list
+
+  validate :num, :presence
+  validate :num, :format, NUMBER_FORMAT
+  validate :attribute_class, :type, 'Train'
+
+  def initialize(number)
+    @number = number
+    @speed = 0
+    @type = nil
+    @route = nil
+    @wagons_list = []
+    register_instance
+    @@all_trains[number] = self
+    validate!
+    puts "Вы создали поезд номер #{number}" if valid? == true
+  end
+
+  def wagons_list_actions(&block)
+    @wagons_list.each(&block)
+  end
+
+  def acselerate(speed)
+    @speed += speed if speed.positive?
+  end
+
+  def valid?
+    validate!
+    true
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def self.find(number)
+    @@all_trains[number]
+  end
+
+  def slowdown(speed)
+    return @speed.zero? if speed > @speed
+  end
+
+  def route_set(route)
+    @route = route
+    @station_index = 0
+    @current_station = @route.route[@station_index]
+  end
+
+  def move_forward
+    return unless @current_station == @route.finish
+
+    @current_station = @route.stations.find_index(@station_index + 1)
+  end
+
+  def move_back
+    return unless @current_station == @route.start
+
+    @current_station = @route.stations.find_index(@station_index - 1)
+  end
+
+  def next_station
+    @route.route[@station_index + 1]
+  end
+
+  def prev_station
+    return if @station_index < 1
+
+    @route.route[@station_index - 1]
+  end
+
+  def add_wagons(wagon)
+    if @speed.zero? && @type == wagon.type
+      @wagons_list.push(wagon)
+    else
+      puts 'Пожалуйста,остановите поезд перед добавлением вагонов и проверьте тип вагона'
+    end
+  end
+
+  def remove_wagons(wagon)
+    if @speed.zero? && @wagons_list.empty? == false
+      @wagons_list.delete(wagon)
+    else
+      puts 'Пожалуйста,остановите поезд и проверьте наличие вагонов'
+    end
+  end
+
+  protected
+
+  def validate!
+    raise 'Номер поезда не был введен' if number.nil?
+    raise 'Некорректный формат номера поезда' if number !~ NUMBER_FORMAT
+  end
+end
